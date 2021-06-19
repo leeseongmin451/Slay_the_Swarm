9f26d7dcd510f56a2c7670f5ed42c137b9449f1d
leeseongmin451 <leeseongmin451@gmail.com>
2021-06-19 23:34:08 +0900

    Add code for setting game title and icon

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ pygame.init()
 # Create the screen
 screen = pygame.display.set_mode((1200, 800))
 
-running = True
+# Game title and icon
+icon = pygame.image.load("icon.png")
+pygame.display.set_icon(icon)                   # Set icon
+pygame.display.set_caption("Slay the Swarm")    # Set title
+
+
+# Main game loop
+running = True      # Variable to check whether continue game
 while running:
+
+    # Get all events occurred during the game
     for event in pygame.event.get():
+        # Check whether quit button clicked
         if event.type == pygame.QUIT:
             running = False
