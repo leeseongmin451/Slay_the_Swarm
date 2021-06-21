1ff1c318bc57f09fa89a96eefefbb0615aab2322
leeseongmin451 <leeseongmin451@gmail.com>
2021-06-21 14:26:57 +0900

    Add update method in Player class for moving player using WASD keys
     - This method accelerates player while key pressed until reach max speed.
     - Add rms function to calculate current speed and compare to max speed
     - Add code for update sprite group including player instance

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import pygame
+import math
 
 
 # Initialize the pygame
 pygame.init()
+
+
+def rms(*args):
+    """
+    Returns root-mean-square of input values.
+    :param args:
+    :return:
+    """
+
+    return math.sqrt(sum([x*x for x in args]))
 
 
 # Player sprite
@@ -42,6 +53,46 @@ class Player(pygame.sprite.Sprite):
         # Attributes for weapons
         self.weapons = []
 
+    def update(self):
+        """
+        Update function for moving player sprite, using weapons per frame
+        :return: None
+        """
+
+        # Accelerate player with WASD key
+        keys = pygame.key.get_pressed()
+        current_speed = rms(self.x_speed, self.y_speed)
+
+        if keys[pygame.K_w]:        # Accelerate upwards with key "w"
+            if current_speed < self.max_speed:
+                self.y_speed -= self.acc
+        elif self.y_speed < 0:
+            self.y_speed += self.acc
+
+        if keys[pygame.K_s]:        # Accelerate downwards with key "s"
+            if current_speed < self.max_speed:
+                self.y_speed += self.acc
+        elif self.y_speed > 0:
+            self.y_speed -= self.acc
+
+        if keys[pygame.K_a]:        # Accelerate left with key "a"
+            if current_speed < self.max_speed:
+                self.x_speed -= self.acc
+        elif self.x_speed < 0:
+            self.x_speed += self.acc
+
+        if keys[pygame.K_d]:        # Accelerate right with key "d"
+            if current_speed < self.max_speed:
+                self.x_speed += self.acc
+        elif self.x_speed > 0:
+            self.x_speed -= self.acc
+
+        # Move the position of player according to current speed (per fps)
+        # And set the actual position on the screen
+        self.x_pos += self.x_speed / fps
+        self.y_pos += self.y_speed / fps
+        self.rect.topleft = [round(self.x_pos), round(self.y_pos)]
+
 
 # Create the screen
 screen = pygame.display.set_mode((1200, 800))
@@ -72,6 +123,9 @@ while running:
         # Check whether quit button clicked
         if event.type == pygame.QUIT:
             running = False
+
+    # Update all sprites
+    all_sprites.update()
 
     # All colors will be represented with RGB tuple (r, g, b)
     screen.fill((0, 0, 0))      # fill the screen background with black(0, 0, 0) before drawing all other sprites
